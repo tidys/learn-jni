a886a3518683f2750e5c1c2534f745cab488348b
cc <xu_yanfeng@126.com>
2023-03-08 15:04:11 +0800

    1

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,6 +8,6 @@ JNICALL
 Java_com_example_learnndk_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C++！";
     return env->NewStringUTF(hello.c_str());
 }
